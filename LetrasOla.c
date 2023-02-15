c599e2021664885a8de0971eefe035e71640a170
lucas27716 <a27716@tomazpelayo.com>
2023-02-15 14:24:09 +0000

    exercicio

--- LetrasOla.c
+++ LetrasOla.c
@@ -1,3 +1,12 @@
 // Resolva neste ficheiro o seu exercício
+#include <stdio.h>
 
+int main()
+{
+    printf("  ####    #          ##\n");
+    printf(" #    #   #         #  #\n");
+    printf(" #    #   #        ######\n");
+    printf("  ####    ######  #      #");
+    return (0);
+}
 
